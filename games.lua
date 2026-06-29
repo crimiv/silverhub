d4313a3181cd72811c5dcc9674857f5d9df7bdfb
coolio <bj6503735@gmail.com>
2026-06-29 10:46:01 -0500

    Update games.lua

--- games.lua
+++ games.lua
@@ -2,4 +2,6 @@ return {
     [142823291] = "games/mm2/init.lua", -- Murder Mystery 2
     [6961824067] = "games/ftap/init.lua", -- Fling Things and People
     [12645083079] = "games/gs/init.lua", -- Giant Simulator
+
+    fallback = "games/universal/init.lua"
 }
